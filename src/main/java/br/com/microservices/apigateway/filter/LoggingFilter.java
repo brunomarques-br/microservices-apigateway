43e0842e394dbe12aa9a4e447a252ac4a9d9af47
Bruno Marques <brunomcamilo.br@gmail.com>
2024-07-24 15:45:20 -0300

    Using "Logging Filter" with Spring Cloud Gateway

---
+++ src/main/java/br/com/microservices/apigateway/filter/LoggingFilter.java
@@ -0,0 +1,28 @@
+package br.com.microservices.apigateway.filter;
+
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.logging.Logger;
+
+
+/*
+*
+* Objective: Log importants information
+*
+* */
+@Component
+public class LoggingFilter implements GlobalFilter {
+
+    private final Logger logger = Logger.getLogger(LoggingFilter.class.getName());
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        logger.info("Original request path: " + exchange.getRequest().getPath());
+        return chain.filter(exchange);
+    }
+
+}
